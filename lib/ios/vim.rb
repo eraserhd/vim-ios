c61b1d7e91e86c627cd575e90054a702bab34b61
Jason Felice <jason.m.felice@gmail.com>
2011-10-20 22:23:52 -0400

    Add :A mapping

--- lib/ios/vim.rb
+++ lib/ios/vim.rb
@@ -4,7 +4,15 @@ require 'ios/vim/filename'
 
 module IOS
   module Vim
+
     def self.initialize
+      VIM.command 'autocmd FileType objc command -buffer A :ruby IOS::Vim::alternate<CR>'
+    end
+
+    def self.alternate
+      filename = VIM::Buffer.current.name
+      new_file = AlternateFinder.new(FileClassifier.new(filename)).alternate
+      VIM.command "edit #{new_file}"
     end
   end 
 end
