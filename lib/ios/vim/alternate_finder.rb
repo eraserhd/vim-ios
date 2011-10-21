01bd7538e16d78a55a078d85d131f1c406863a64
Jason Felice <jason.m.felice@gmail.com>
2011-10-20 20:09:05 -0400

    Find the alternate for a .m file

--- lib/ios/vim/alternate_finder.rb
+++ lib/ios/vim/alternate_finder.rb
@@ -1,6 +1,9 @@
 module IOS
   module Vim
     class AlternateFinder
+      def alternate_for filename
+        filename.gsub(/\.m/, ".h")
+      end
     end
   end
 end
--- spec/ios/vim/alternate_finder_spec.rb
+++ spec/ios/vim/alternate_finder_spec.rb
@@ -1,4 +1,11 @@
 require 'ios/vim/alternate_finder'
 
 describe IOS::Vim::AlternateFinder do
+
+  subject { IOS::Vim::AlternateFinder.new }
+
+  it "should find a .h from a .m file" do
+    subject.alternate_for('foo/bar.m').should == 'foo/bar.h'
+  end
+
 end
