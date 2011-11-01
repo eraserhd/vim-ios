5c16c0116a4a22462af639ab83cd094981a4730f
Jason Felice <jason.m.felice@gmail.com>
2011-10-31 23:03:48 -0400

    Refactor tests

--- spec/ios/vim_spec.rb
+++ spec/ios/vim_spec.rb
@@ -9,24 +9,16 @@ describe IOS::Vim do
   end
 
   describe '::initialize' do
-    it 'should map the :A command' do
-      VIM.should_receive(:command).with('autocmd FileType objc,objcpp command! -buffer A :ruby IOS::Vim::command_A<CR>')
-      IOS::Vim::initialize
-    end
-
-    it 'should map the :AV command' do
-      VIM.should_receive(:command).with('autocmd FileType objc,objcpp command! -buffer AV :ruby IOS::Vim::command_AV<CR>')
+    it 'should install detected commands' do
+      IOS::Vim.stub(:commands).and_return([:FoObAr])
+      VIM.should_receive(:command).with('autocmd FileType objc,objcpp command! -buffer FoObAr :ruby IOS::Vim::command_FoObAr<CR>')
       IOS::Vim::initialize
     end
   end
 
   describe '::commands' do
-    it 'should return a command for class method named command_*' do
-      IOS::Vim.instance_eval do
-        class<<self
-          def command_QwErTyUiOp; end
-        end
-      end
+    it 'should return :QwErTyUiOp if command_QwErTyUiOp is defined' do
+      IOS::Vim.instance_eval {class<<self; def command_QwErTyUiOp; end; end}
       IOS::Vim.send(:commands).should include(:QwErTyUiOp)
     end
   end
