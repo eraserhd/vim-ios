c8e166a04de610145f782ba8054cbedfdf694d3a
Jason Felice <jason.m.felice@gmail.com>
2011-10-31 22:39:32 -0400

    Move tests for IOS::Vim::Commands into commands_spec

---
+++ spec/ios/vim/commands_spec.rb
@@ -0,0 +1,40 @@
+require 'ios/vim/commands'
+require 'ios/vim/alternate_finder'
+
+module VIM; class Buffer; end; end
+
+describe IOS::Vim::Commands do
+
+  subject do
+    o = Object.new
+    o.extend IOS::Vim::Commands
+    o
+  end
+
+  describe '::command_A for some_file.h' do
+    it "emits an ':edit some_file.mm' command" do
+      VIM::Buffer.stub_chain(:current, :name).and_return('this_shouldnt_exist.h')
+      subject.stub(:alternate_file_for).and_return('this_shouldnt_exist.mm')
+      VIM.should_receive(:command).with('edit this_shouldnt_exist.mm')
+      subject.command_A
+    end
+  end
+
+  describe '::command_AV for some_file.h' do
+    it "emits an ':vsplit some_file.mm' command" do
+      VIM::Buffer.stub_chain(:current, :name).and_return('this_shouldnt_exist.h')
+      subject.stub(:alternate_file_for).and_return('this_shouldnt_exist.mm')
+      VIM.should_receive(:command).with('vsplit this_shouldnt_exist.mm')
+      subject.command_AV
+    end
+  end
+
+  describe '::alternate_file_for foo.h' do
+
+    it 'should return what AlternateFinder returns' do
+      IOS::Vim::AlternateFinder.should_receive(:new).with('foo.h').and_return(stub :alternate => 'foo.m')
+      subject.alternate_file_for('foo.h').should == 'foo.m'
+    end
+  end
+
+end
--- spec/ios/vim_spec.rb
+++ spec/ios/vim_spec.rb
@@ -1,8 +1,6 @@
 require 'ios/vim'
 
-module VIM
-  class Buffer; end
-end
+module VIM; class Buffer; end; end
 
 describe IOS::Vim do
 
@@ -20,34 +18,6 @@ describe IOS::Vim do
       VIM.should_receive(:command).with('autocmd FileType objc,objcpp command! -buffer AV :ruby IOS::Vim::command_AV<CR>')
       IOS::Vim::initialize
     end
-  end
-
-  describe '::command_A for some_file.h' do
-    it "emits an ':edit some_file.mm' command" do
-      VIM::Buffer.stub_chain(:current, :name).and_return('this_shouldnt_exist.h')
-      IOS::Vim.stub(:alternate_file_for).and_return('this_shouldnt_exist.mm')
-      VIM.should_receive(:command).with('edit this_shouldnt_exist.mm')
-      IOS::Vim::command_A
-    end
-  end
-
-  describe '::command_AV for some_file.h' do
-    it "emits an ':vsplit some_file.mm' command" do
-      VIM::Buffer.stub_chain(:current, :name).and_return('this_shouldnt_exist.h')
-      IOS::Vim.stub(:alternate_file_for).and_return('this_shouldnt_exist.mm')
-      VIM.should_receive(:command).with('vsplit this_shouldnt_exist.mm')
-      IOS::Vim::command_AV
-    end
-  end
-
-  describe '::alternate_file_for foo.h' do
-    before do
-      IOS::Vim::AlternateFinder.should_receive(:new).with('foo.h').and_return(stub :alternate => 'foo.m')
-    end
-
-    subject { IOS::Vim.alternate_file_for('foo.h') }
-    
-    it { should == 'foo.m' }
   end
 
 end
