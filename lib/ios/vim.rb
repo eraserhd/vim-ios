707d4f5079f2640e14cec49754c524b8b27113a9
Jason Felice <jason.m.felice@gmail.com>
2011-10-31 21:41:16 -0400

    Move commands into Commands module

--- lib/ios/vim.rb
+++ lib/ios/vim.rb
@@ -9,20 +9,26 @@ module IOS
       VIM.command 'autocmd FileType objc,objcpp command! -buffer A :ruby IOS::Vim::command_A<CR>'
       VIM.command 'autocmd FileType objc,objcpp command! -buffer AV :ruby IOS::Vim::command_AV<CR>'
     end
+
+    module Commands
     
-    def self.command_A
-      new_file = alternate_file_for VIM::Buffer.current.name  
-      VIM.command "edit #{new_file}"
+      def command_A
+        new_file = alternate_file_for VIM::Buffer.current.name  
+        VIM.command "edit #{new_file}"
+      end
+
+      def command_AV
+        new_file = alternate_file_for VIM::Buffer.current.name  
+        VIM.command "vsplit #{new_file}"
+      end
+
+      def alternate_file_for(filename)
+        AlternateFinder.new(filename).alternate
+      end
+
     end
 
-    def self.command_AV
-      new_file = alternate_file_for VIM::Buffer.current.name  
-      VIM.command "vsplit #{new_file}"
-    end
-
-    def self.alternate_file_for(filename)
-      AlternateFinder.new(filename).alternate
-    end
+    extend Commands
 
   end 
 end
