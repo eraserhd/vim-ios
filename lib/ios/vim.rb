058e141bedf8447abb5156a4d54a6ea083fd5a10
Jason Felice <jason.m.felice@gmail.com>
2011-10-31 21:38:14 -0400

    Implement :AV command

--- lib/ios/vim.rb
+++ lib/ios/vim.rb
@@ -7,11 +7,17 @@ module IOS
 
     def self.initialize
       VIM.command 'autocmd FileType objc,objcpp command! -buffer A :ruby IOS::Vim::alternate<CR>'
+      VIM.command 'autocmd FileType objc,objcpp command! -buffer AV :ruby IOS::Vim::command_AV<CR>'
     end
-
+    
     def self.alternate
       new_file = alternate_file_for VIM::Buffer.current.name  
       VIM.command "edit #{new_file}"
+    end
+
+    def self.command_AV
+      new_file = alternate_file_for VIM::Buffer.current.name  
+      VIM.command "vsplit #{new_file}"
     end
 
     def self.alternate_file_for(filename)
--- spec/ios/vim_spec.rb
+++ spec/ios/vim_spec.rb
@@ -6,9 +6,18 @@ end
 
 describe IOS::Vim do
 
+  before do
+    VIM.stub(:command)
+  end
+
   describe '::initialize' do
     it 'should map the :A command' do
       VIM.should_receive(:command).with('autocmd FileType objc,objcpp command! -buffer A :ruby IOS::Vim::alternate<CR>')
+      IOS::Vim::initialize
+    end
+
+    it 'should map the :AV command' do
+      VIM.should_receive(:command).with('autocmd FileType objc,objcpp command! -buffer AV :ruby IOS::Vim::command_AV<CR>')
       IOS::Vim::initialize
     end
   end
@@ -19,6 +28,15 @@ describe IOS::Vim do
       IOS::Vim.stub(:alternate_file_for).and_return('this_shouldnt_exist.mm')
       VIM.should_receive(:command).with('edit this_shouldnt_exist.mm')
       IOS::Vim::alternate
+    end
+  end
+
+  describe '::command_AV for some_file.h' do
+    it "emits an ':vsplit some_file.mm' command" do
+      VIM::Buffer.stub_chain(:current, :name).and_return('this_shouldnt_exist.h')
+      IOS::Vim.stub(:alternate_file_for).and_return('this_shouldnt_exist.mm')
+      VIM.should_receive(:command).with('vsplit this_shouldnt_exist.mm')
+      IOS::Vim::command_AV
     end
   end
 
