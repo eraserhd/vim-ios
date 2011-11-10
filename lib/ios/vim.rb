563409f96ee93efe5fb1844936e7b77849b6323f
Jason Felice <jason.m.felice@gmail.com>
2011-11-09 23:01:58 -0500

    Implement IOS::Vim#install_edit_command

--- lib/ios/vim.rb
+++ lib/ios/vim.rb
@@ -28,6 +28,20 @@ module IOS
       end
       private :map_buffer_command
 
+      def install_edit_command(command)
+        VIM.command "autocmd FileType objc,objcpp command! -buffer #{command} :ruby IOS::Vim::edit_command_#{command}('edit')<CR>"
+        VIM.command "autocmd FileType objc,objcpp command! -buffer #{edit_command_variant command, 'E'} :ruby IOS::Vim::edit_command_#{command}('edit')<CR>"
+        VIM.command "autocmd FileType objc,objcpp command! -buffer #{edit_command_variant command, 'V'} :ruby IOS::Vim::edit_command_#{command}('vsplit')<CR>"
+        VIM.command "autocmd FileType objc,objcpp command! -buffer #{edit_command_variant command, 'S'} :ruby IOS::Vim::edit_command_#{command}('split')<CR>"
+        VIM.command "autocmd FileType objc,objcpp command! -buffer #{edit_command_variant command, 'T'} :ruby IOS::Vim::edit_command_#{command}('tabedit')<CR>"
+      end
+      private :install_edit_command
+
+      def edit_command_variant(command, letter)
+        "#{command[0..0]}#{letter}#{command[1..-1]}"
+      end
+      private :edit_command_variant
+
     end
 
     extend Commands
--- spec/ios/vim_spec.rb
+++ spec/ios/vim_spec.rb
@@ -37,4 +37,16 @@ describe IOS::Vim do
     end
   end
 
+  describe '::install_edit_command' do
+    it 'should map the command for each edit "type"' do
+      VIM.stub(:command)
+      VIM.should_receive(:command).with("autocmd FileType objc,objcpp command! -buffer Foobar :ruby IOS::Vim::edit_command_Foobar('edit')<CR>")
+      VIM.should_receive(:command).with("autocmd FileType objc,objcpp command! -buffer FEoobar :ruby IOS::Vim::edit_command_Foobar('edit')<CR>")
+      VIM.should_receive(:command).with("autocmd FileType objc,objcpp command! -buffer FVoobar :ruby IOS::Vim::edit_command_Foobar('vsplit')<CR>")
+      VIM.should_receive(:command).with("autocmd FileType objc,objcpp command! -buffer FSoobar :ruby IOS::Vim::edit_command_Foobar('split')<CR>")
+      VIM.should_receive(:command).with("autocmd FileType objc,objcpp command! -buffer FToobar :ruby IOS::Vim::edit_command_Foobar('tabedit')<CR>")
+      IOS::Vim.send :install_edit_command, :Foobar
+    end
+  end
+
 end
