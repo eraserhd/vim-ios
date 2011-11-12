f344c6924b6cdf187252b655a03e974cdb0fc2bf
Jason Felice <jason.m.felice@gmail.com>
2011-11-12 14:50:32 -0500

    RelatedFinder#spec works if spec given

--- lib/ios/vim/related_finder.rb
+++ lib/ios/vim/related_finder.rb
@@ -2,6 +2,15 @@ module IOS
   module Vim
 
     class RelatedFinder
+
+      def initialize(filename)
+        @filename = filename
+      end
+
+      def spec
+        @filename
+      end
+      
     end
 
   end
--- spec/ios/vim/related_finder_spec.rb
+++ spec/ios/vim/related_finder_spec.rb
@@ -1,4 +1,13 @@
 require 'ios/vim'
 
 describe IOS::Vim::RelatedFinder do
+
+  describe '#spec' do
+
+    it 'returns the current file if it is a spec' do
+      IOS::Vim::RelatedFinder.new('foo/BarSpec.m').spec.should == 'foo/BarSpec.m'
+    end
+
+  end
+
 end
