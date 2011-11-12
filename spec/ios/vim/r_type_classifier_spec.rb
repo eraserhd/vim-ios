19352d1f6cacac47c7078cb50baad1a52fd2d7e3
Jason Felice <jason.m.felice@gmail.com>
2011-11-12 00:12:16 -0500

    Refactor RTypeClassifier test

--- spec/ios/vim/r_type_classifier_spec.rb
+++ spec/ios/vim/r_type_classifier_spec.rb
@@ -2,16 +2,20 @@ require 'ios/vim'
 
 describe IOS::Vim::RTypeClassifier do
 
-  it "should classify '*Spec.*' files as :spec" do
-    IOS::Vim::RTypeClassifier.new('FooClassSpec.cXfObbA__r').type.should == :spec
-  end
+  describe '#type' do
 
-  it "should classify '*Test.*' files as :spec" do
-    IOS::Vim::RTypeClassifier.new('FooClassTest.llNOTq').type.should == :spec
-  end
+    it "classifies '*Spec.*' files as :spec" do
+      IOS::Vim::RTypeClassifier.new('FooClassSpec.cXfObbA__r').type.should == :spec
+    end
 
-  it "should classify other files as :code" do
-    IOS::Vim::RTypeClassifier.new('FooClass.m').type.should == :code
+    it "classifies '*Test.*' files as :spec" do
+      IOS::Vim::RTypeClassifier.new('FooClassTest.llNOTq').type.should == :spec
+    end
+
+    it "classifies other files as :code" do
+      IOS::Vim::RTypeClassifier.new('FooClass.m').type.should == :code
+    end
+
   end
 
 end
