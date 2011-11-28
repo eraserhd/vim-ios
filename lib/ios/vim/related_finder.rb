ef085f22b1a65fbed3d53c4affd3c5d7ee2a8bdc
Jason Felice <jason.m.felice@gmail.com>
2011-11-27 21:15:37 -0500

    Extract RelatedFinder::ImplFinder class

--- lib/ios/vim/related_finder.rb
+++ lib/ios/vim/related_finder.rb
@@ -38,24 +38,35 @@ module IOS
         SpecFinder.new(@filename, @classifier).find
       end
 
+      class ImplFinder
+        def initialize(filename, classifier)
+          @filename = filename
+          @classifier = classifier
+        end
+
+        def find
+          return @filename if @classifier.type == :impl
+          impl_candidates.detect{|e| File.exists? e} || default_impl
+        end
+
+        def impl_candidates
+          [
+            "#{@classifier.stem}.mm",
+            default_impl,
+            "#{@classifier.stem}.h"
+          ]
+        end
+        private :impl_candidates
+
+        def default_impl
+          "#{@classifier.stem}.m"
+        end
+        private :default_impl
+      end
+
       def impl
-        return @filename if @classifier.type == :impl
-        impl_candidates.detect{|e| File.exists? e} || default_impl
+        ImplFinder.new(@filename, @classifier).find
       end
-
-      def impl_candidates
-        [
-          "#{@classifier.stem}.mm",
-          default_impl,
-          "#{@classifier.stem}.h"
-        ]
-      end
-      private :impl_candidates
-
-      def default_impl
-        "#{@classifier.stem}.m"
-      end
-      private :default_impl
 
     end
 
