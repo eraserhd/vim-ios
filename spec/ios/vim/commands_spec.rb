5865a9a9df53d3dd66e908c2cb0c0d143f84b9b5
Jason Felice <jason.m.felice@gmail.com>
2011-11-27 20:38:31 -0500

    Extract #should_edit_found_file test helper

--- spec/ios/vim/commands_spec.rb
+++ spec/ios/vim/commands_spec.rb
@@ -18,13 +18,18 @@ describe IOS::Vim::Commands do
 
   def finder(kind, stub_params)
     kind.should_receive(:new).with(@filename).and_return(stub stub_params)
+    @file_to_edit = stub_params.values.first
+  end
+
+  def should_edit_found_file
+    VIM.should_receive(:command).with("foozle #{@file_to_edit}")
   end
 
   describe ':A' do
     it "emits the edit command for the alternate" do
       when_editing 'FooFile.m'
       finder IOS::Vim::AlternateFinder, :alternate => 'FooFile.h'
-      VIM.should_receive(:command).with("foozle FooFile.h")
+      should_edit_found_file
       subject.edit_command_A 'foozle'
     end
   end
@@ -33,8 +38,8 @@ describe IOS::Vim::Commands do
     it "emits the edit command for the impl found by the related file finder" do
       when_editing 'FooSpec.m'
       finder IOS::Vim::RelatedFinder, :impl => 'Foo.m'
-      VIM.should_receive(:command).with("barzle Foo.m")
-      subject.edit_command_Rimpl 'barzle'
+      should_edit_found_file
+      subject.edit_command_Rimpl 'foozle'
     end
   end
 
@@ -42,7 +47,7 @@ describe IOS::Vim::Commands do
     it "edits the spec found by the related file finder" do
       when_editing 'FooClass.m'
       finder IOS::Vim::RelatedFinder, :spec => 'FooClassSpec.m'
-      VIM.should_receive(:command).with("foozle FooClassSpec.m")
+      should_edit_found_file
       subject.edit_command_Rspec 'foozle'
     end
   end
