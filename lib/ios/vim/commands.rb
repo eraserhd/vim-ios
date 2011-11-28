2d4a8d6a4c9c4d8bdbf5c4d6e2351486369004f5
Jason Felice <jason.m.felice@gmail.com>
2011-11-27 20:17:53 -0500

    Inline Commands#spec_for

--- lib/ios/vim/commands.rb
+++ lib/ios/vim/commands.rb
@@ -9,17 +9,12 @@ module IOS
       end
 
       def edit_command_Rspec(method)
-        VIM.command "#{method} #{spec_for VIM::Buffer.current.name}"
+        VIM.command "#{method} #{RelatedFinder.new(VIM::Buffer.current.name).spec}"
       end
       
       def edit_command_Rimpl(method)
         VIM.command "#{method} #{RelatedFinder.new(VIM::Buffer.current.name).impl}"
       end
-
-      def spec_for(filename)
-        RelatedFinder.new(filename).spec
-      end
-      private :spec_for
 
       def alternate_file_for(filename)
         AlternateFinder.new(filename).alternate
