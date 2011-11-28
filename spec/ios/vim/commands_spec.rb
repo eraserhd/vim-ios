546e9255afe5d46c408817e8b2d81fd3b4b113fd
Jason Felice <jason.m.felice@gmail.com>
2011-11-27 20:09:00 -0500

    Don't test private method

--- spec/ios/vim/commands_spec.rb
+++ spec/ios/vim/commands_spec.rb
@@ -18,12 +18,12 @@ describe IOS::Vim::Commands do
 
     before do
       VIM::Buffer.stub_chain(:current, :name).and_return original_file
-      subject.stub(:alternate_file_for).and_return expected_alternate
     end
 
     describe ':A' do
       it "emits the edit command for the alternate" do
         VIM.should_receive(:command).with("foozle #{expected_alternate}")
+        IOS::Vim::AlternateFinder.should_receive(:new).with(original_file).and_return(stub :alternate => expected_alternate)
         subject.edit_command_A 'foozle'
       end
     end
@@ -40,16 +40,9 @@ describe IOS::Vim::Commands do
   describe ':Rspec' do
     it "edits the spec found by the related file finder" do
       VIM::Buffer.stub_chain(:current, :name).and_return('FooClass.m')
-      VIM.should_receive(:command).with("foozle FooClassSpec.m")
       IOS::Vim::RelatedFinder.should_receive(:new).with('FooClass.m').and_return(stub :spec => 'FooClassSpec.m')
+      VIM.should_receive(:command).with("foozle FooClassSpec.m")
       subject.edit_command_Rspec 'foozle'
-    end
-  end
-
-  describe '::alternate_file_for foo.h' do
-    it 'should return what AlternateFinder returns' do
-      IOS::Vim::AlternateFinder.should_receive(:new).with('foo.h').and_return(stub :alternate => 'foo.m')
-      subject.send(:alternate_file_for,'foo.h').should == 'foo.m'
     end
   end
 
