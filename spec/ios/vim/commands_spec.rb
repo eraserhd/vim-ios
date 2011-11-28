30bb59533488a588824e230b9b60eaf72a9d5086
Jason Felice <jason.m.felice@gmail.com>
2011-11-27 20:23:40 -0500

    Extract #editing test helper

--- spec/ios/vim/commands_spec.rb
+++ spec/ios/vim/commands_spec.rb
@@ -11,9 +11,13 @@ describe IOS::Vim::Commands do
     o
   end
 
+  def editing(filename)
+    VIM::Buffer.stub_chain(:current, :name).and_return filename
+  end
+
   describe ':A' do
     it "emits the edit command for the alternate" do
-      VIM::Buffer.stub_chain(:current, :name).and_return 'FooFile.m'
+      editing 'FooFile.m'
       IOS::Vim::AlternateFinder.should_receive(:new).with('FooFile.m').and_return(stub :alternate => 'FooFile.h')
       VIM.should_receive(:command).with("foozle FooFile.h")
       subject.edit_command_A 'foozle'
@@ -22,7 +26,7 @@ describe IOS::Vim::Commands do
 
   describe ':Rimpl' do
     it "emits the edit command for the impl found by the related file finder" do
-      VIM::Buffer.stub_chain(:current, :name).and_return 'FooSpec.m'
+      editing 'FooSpec.m'
       IOS::Vim::RelatedFinder.should_receive(:new).with('FooSpec.m').and_return(stub :impl => 'Foo.m')
       VIM.should_receive(:command).with("barzle Foo.m")
       subject.edit_command_Rimpl 'barzle'
@@ -31,7 +35,7 @@ describe IOS::Vim::Commands do
 
   describe ':Rspec' do
     it "edits the spec found by the related file finder" do
-      VIM::Buffer.stub_chain(:current, :name).and_return('FooClass.m')
+      editing 'FooClass.m'
       IOS::Vim::RelatedFinder.should_receive(:new).with('FooClass.m').and_return(stub :spec => 'FooClassSpec.m')
       VIM.should_receive(:command).with("foozle FooClassSpec.m")
       subject.edit_command_Rspec 'foozle'
