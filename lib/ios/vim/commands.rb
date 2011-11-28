00247319d52aa2bb836abf9ff6377f37353d0047
Jason Felice <jason.m.felice@gmail.com>
2011-11-27 20:19:01 -0500

    Inline #alternate_file_for

--- lib/ios/vim/commands.rb
+++ lib/ios/vim/commands.rb
@@ -5,7 +5,7 @@ module IOS
     module Commands
 
       def edit_command_A(method)
-        open_alternate method
+        VIM.command "#{method} #{AlternateFinder.new(VIM::Buffer.current.name).alternate}"
       end
 
       def edit_command_Rspec(method)
@@ -15,17 +15,6 @@ module IOS
       def edit_command_Rimpl(method)
         VIM.command "#{method} #{RelatedFinder.new(VIM::Buffer.current.name).impl}"
       end
-
-      def alternate_file_for(filename)
-        AlternateFinder.new(filename).alternate
-      end
-      private :alternate_file_for
-
-      def open_alternate(command)
-        new_file = alternate_file_for VIM::Buffer.current.name  
-        VIM.command "#{command} #{new_file}"
-      end
-      private :open_alternate
 
     end
 
