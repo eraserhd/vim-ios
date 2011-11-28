64f8ec2ea8001b7266261f8bacf70b9a3cc08f6b
Jason Felice <jason.m.felice@gmail.com>
2011-11-27 20:02:59 -0500

    Describe using command names

--- spec/ios/vim/commands_spec.rb
+++ spec/ios/vim/commands_spec.rb
@@ -21,14 +21,14 @@ describe IOS::Vim::Commands do
       subject.stub(:alternate_file_for).and_return expected_alternate
     end
 
-    describe '::edit_command_A' do
+    describe ':A' do
       it "emits the edit command for the alternate" do
         VIM.should_receive(:command).with("foozle #{expected_alternate}")
         subject.edit_command_A 'foozle'
       end
     end
 
-    describe '::edit_command_Rimpl' do
+    describe ':Rimpl' do
       it "emits the edit command for the same file" do
         VIM.should_receive(:command).with("barzle #{original_file}")
         subject.edit_command_Rimpl 'barzle'
@@ -37,7 +37,7 @@ describe IOS::Vim::Commands do
 
   end
 
-  describe '::edit_command_Rspec' do
+  describe ':Rspec' do
     it "edits the spec found by the related file finder" do
       VIM::Buffer.stub_chain(:current, :name).and_return('FooClass.m')
       VIM.should_receive(:command).with("foozle FooClassSpec.m")
