561fc640ade6da10bcac58826058397c8757fd99
Jason Felice <jason.m.felice@gmail.com>
2011-12-13 21:53:42 -0500

    Extract #at_topmost_directory? #projects_in_current_directory

--- lib/ios/vim/xcode_project_finder.rb
+++ lib/ios/vim/xcode_project_finder.rb
@@ -10,17 +10,27 @@ module IOS
       def find
         @directory = @directory_lister.current_directory
         while true
-          files = @directory_lister.list(@directory).select{|path| path =~ /\.xcodeproj$/i}
+          files = projects_in_current_directory
           unless files.empty?
             return XcodeProject.new File.join(@directory,files.first)
           end
 
-          break if File.dirname(@directory) == @directory
+          break if at_topmost_directory?
           @directory = File.dirname(@directory)
         end
 
         nil
       end
+
+      def projects_in_current_directory
+        @directory_lister.list(@directory).select{|path| path =~ /\.xcodeproj$/i}
+      end
+      private :projects_in_current_directory
+
+      def at_topmost_directory?
+        File.dirname(@directory) == @directory
+      end
+      private :at_topmost_directory?
 
     end
 
