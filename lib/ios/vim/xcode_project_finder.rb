ec406878cde1c5590b1af2e249283e4b754206d6
Jason Felice <jason.m.felice@gmail.com>
2011-12-13 21:29:35 -0500

    Find .xcodeproj in parent directory

--- lib/ios/vim/xcode_project_finder.rb
+++ lib/ios/vim/xcode_project_finder.rb
@@ -8,9 +8,18 @@ module IOS
       end
 
       def find
-        files = @filesystem.list('.').select{|path| path =~ /\.xcodeproj$/i}
-        return nil if files.empty?
-        XcodeProject.new files.first
+        directory = @filesystem.current_directory
+        while true
+          files = @filesystem.list(directory).select{|path| path =~ /\.xcodeproj$/i}
+          unless files.empty?
+            return XcodeProject.new File.join(directory,files.first)
+          end
+
+          break if File.dirname(directory) == directory
+          directory = File.dirname(directory)
+        end
+
+        nil
       end
 
     end
--- spec/ios/vim/xcodeproj_finder_spec.rb
+++ spec/ios/vim/xcodeproj_finder_spec.rb
@@ -4,7 +4,9 @@ describe IOS::Vim::XcodeProjectFinder do
 
   context "when it can't find a project folder" do
 
-    let!(:filesystem) {double :list => []}
+    let!(:filesystem) do
+      double :current_directory => '/Foo/bar', :list => []
+    end
     subject {IOS::Vim::XcodeProjectFinder.new filesystem}
 
     it "returns nil" do
@@ -16,18 +18,34 @@ describe IOS::Vim::XcodeProjectFinder do
   context "when there's a .xcodeproj in the current directory" do
 
     let!(:filesystem) do
-      filesystem = double
-      filesystem.stub(:list).with('.').and_return(['Foo.xcodeproj'])
+      filesystem = double(:current_directory => '/Foo/bar/baz')
+      filesystem.stub(:list).with('/Foo/bar/baz').and_return(['Foo.xcodeproj'])
       filesystem
     end
     subject {IOS::Vim::XcodeProjectFinder.new filesystem}
 
     it "returns an XcodeProject with the correct path" do
       subject.find.should be_kind_of(IOS::Vim::XcodeProject)
-      subject.find.path.should == "Foo.xcodeproj"
+      subject.find.path.should == "/Foo/bar/baz/Foo.xcodeproj"
+    end
+
+  end
+
+  context "when there's a .xcodeproj in a parent directory" do
+    
+    let!(:filesystem) do
+      filesystem = double(:current_directory => '/Foo/bar/baz')
+      filesystem.stub(:list).with('/Foo/bar/baz').and_return([])
+      filesystem.stub(:list).with('/Foo/bar').and_return(['Foo.xcodeproj'])
+      filesystem
+    end
+    subject {IOS::Vim::XcodeProjectFinder.new filesystem}
+
+    it "returns an XcodeProject with the correct path" do
+      subject.find.should be_kind_of(IOS::Vim::XcodeProject)
+      subject.find.path.should == "/Foo/bar/Foo.xcodeproj"
     end
 
   end
   
-
 end
