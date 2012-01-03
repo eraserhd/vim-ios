43489c3204d7fa8ec04962a926a1f08ab9fddc1a
Jason Felice <jason.m.felice@gmail.com>
2012-01-02 23:12:22 -0500

    Rename to xcodeproj_interface

--- lib/ios/vim.rb
+++ lib/ios/vim.rb
@@ -3,10 +3,10 @@ require 'ios/vim/alternate_finder'
 require 'ios/vim/command_installer'
 require 'ios/vim/edit_commands'
 require 'ios/vim/filename'
-require 'ios/vim/maybe_zerg'
 require 'ios/vim/r_type_classifier'
 require 'ios/vim/related_finder'
 require 'ios/vim/x_commands'
+require 'ios/vim/xcodeproj_interface'
 
 module IOS
   module Vim
