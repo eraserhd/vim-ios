b68816c7daf77c925cdfe557661798e7f5949e2c
Jason Felice <jason.m.felice@gmail.com>
2012-01-03 22:50:42 -0500

    Simplify XcodeprojInterface::instance

--- lib/ios/vim/xcodeproj_interface.rb
+++ lib/ios/vim/xcodeproj_interface.rb
@@ -6,12 +6,10 @@ module IOS
     class XcodeprojInterface
 
       def self.instance
-        begin
-          gem 'zerg_xcode', '~> 0.4.0'
-          XcodeprojInterface.new
-        rescue LoadError => e
-          raise LoadError.new('This feature requires the zerg_xcode gem.')
-        end
+        gem 'zerg_xcode', '~> 0.4.0'
+        XcodeprojInterface.new
+      rescue LoadError => e
+        raise LoadError.new('This feature requires the zerg_xcode gem.')
       end
     end
 
