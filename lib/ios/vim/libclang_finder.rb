92aff3273bbb3348415c46cd7592da9f22d5ebab
Jason Felice <jason.m.felice@gmail.com>
2012-07-29 16:33:13 -0400

    LibclangFinder parses the output of xcrun

--- lib/ios/vim/libclang_finder.rb
+++ lib/ios/vim/libclang_finder.rb
@@ -3,8 +3,14 @@ module IOS
   module Vim
 
     class LibclangFinder
+      def initialize(command_runner = ShellCommandRunner.new)
+        @command_runner = command_runner
+      end
+
       def find
-        raise IOError.new
+        code, output = @command_runner.run "xcrun -find clang"
+        raise IOError.new unless code == 0
+        File.join(File.dirname(File.dirname(output)),'lib','libclang.dylib')
       end
     end
 
--- spec/ios/vim/libclang_finder_spec.rb
+++ spec/ios/vim/libclang_finder_spec.rb
@@ -1,4 +1,15 @@
 require 'ios/vim'
 
 describe IOS::Vim::LibclangFinder do
+
+  let(:command_runner) {double}
+  subject {IOS::Vim::LibclangFinder.new command_runner}
+
+  context 'when xcrun has zero exit code' do
+    it 'returns a path based on the printed result' do
+      command_runner.stub(:run).and_return [0, "/foo/bar/lib/libclang.dylib"]
+      subject.find.should == "/foo/bar/lib/libclang.dylib"
+    end
+  end
+
 end
