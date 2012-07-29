acacb3acd3d117f65156c13ed6035092103030e6
Jason Felice <jason.m.felice@gmail.com>
2012-07-29 16:14:35 -0400

    ClangCompleterizer sets library path when found

--- lib/ios/vim/clang_completerizer.rb
+++ lib/ios/vim/clang_completerizer.rb
@@ -8,7 +8,19 @@ module IOS
       end
 
       def configure_environment
+        VIM.command configure_command
+      rescue IOError => e
       end
+
+      def configure_command
+        "let g:clang_library_path = #{clang_library_path.inspect}"
+      end
+      private :configure_command
+
+      def clang_library_path
+        File.dirname(@libclang_finder.find)
+      end
+      private :clang_library_path
 
     end
 
--- spec/ios/vim/clang_completerizer_spec.rb
+++ spec/ios/vim/clang_completerizer_spec.rb
@@ -5,7 +5,7 @@ describe IOS::Vim::ClangCompleterizer do
   let(:libclang_finder) {mock}
   subject {IOS::Vim::ClangCompleterizer.new libclang_finder}
 
-  context 'when libclang.dylib is not found' do
+  context 'when libclang is not found' do
     before do
       libclang_finder.stub(:find).and_raise IOError.new
     end
@@ -14,6 +14,18 @@ describe IOS::Vim::ClangCompleterizer do
       VIM.should_not_receive(:command)
       subject.configure_environment
     end
+  end
+
+  context 'when libclang is found' do
+    before do
+      libclang_finder.stub(:find).and_return '/foo/bar/libclang.dylib'
+    end
+
+    it 'sets g:clang_library_path' do
+      VIM.should_receive(:command).with('let g:clang_library_path = "/foo/bar"')
+      subject.configure_environment
+    end
+
   end
 
 end
