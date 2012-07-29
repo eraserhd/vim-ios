eea3db3fbb051ee7b6a53b1582527a15c4a5a106
Jason Felice <jason.m.felice@gmail.com>
2012-07-29 16:38:00 -0400

    Clean up LibclangFinder spec

--- spec/ios/vim/libclang_finder_spec.rb
+++ spec/ios/vim/libclang_finder_spec.rb
@@ -6,9 +6,22 @@ describe IOS::Vim::LibclangFinder do
   subject {IOS::Vim::LibclangFinder.new command_runner}
 
   context 'when xcrun has zero exit code' do
+    before do
+      command_runner.stub(:run).and_return [0, "/foo/bar/bin/clang"]
+    end
+
     it 'returns a path based on the printed result' do
-      command_runner.stub(:run).and_return [0, "/foo/bar/lib/libclang.dylib"]
       subject.find.should == "/foo/bar/lib/libclang.dylib"
+    end
+  end
+
+  context 'when xcrun has a non-zero exit code' do
+    before do
+      command_runner.stub(:run).and_return [42, "???"]
+    end
+
+    it 'raises IOError' do
+      lambda {subject.find}.should raise_error
     end
   end
 
