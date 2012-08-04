80569df5c4fb875656978b095d161b1acdf23236
Jason Felice <jason.m.felice@gmail.com>
2012-08-03 21:31:45 -0400

    Extract some methods in LibclangFinder

--- lib/ios/vim/libclang_finder.rb
+++ lib/ios/vim/libclang_finder.rb
@@ -8,10 +8,25 @@ module IOS
       end
 
       def find
+        File.join(lib_directory, 'libclang.dylib')
+      end
+
+      def path_to_clang
         code, output = @command_runner.run "xcrun -find clang"
         raise IOError.new unless code == 0
-        File.join(File.dirname(File.dirname(output)),'lib','libclang.dylib')
+        output.chop
       end
+      private :path_to_clang
+
+      def clang_directory
+        File.dirname(path_to_clang)
+      end
+      private :clang_directory
+
+      def lib_directory
+        File.join(File.dirname(clang_directory),'lib')
+      end
+      private :lib_directory
     end
 
   end
