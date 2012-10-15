f91f7fb91918308c22e6b3362dad948f6428c090
Jason Felice <jason.m.felice@gmail.com>
2012-10-15 18:34:33 -0400

    Test XcodeBuilder instance as subject

--- spec/ios/vim/xcode_builder_spec.rb
+++ spec/ios/vim/xcode_builder_spec.rb
@@ -6,17 +6,15 @@ describe IOS::Vim::XcodeBuilder do
   it {should respond_to(:build)}
 
   it 'tells us it is building' do
-    builder = IOS::Vim::XcodeBuilder.new
     VIM.stub(:command)
     VIM.should_receive(:command).with('echo "Building... "')
-    builder.build
+    subject.build
   end
 
   it 'tells us when it is done building' do
-    builder = IOS::Vim::XcodeBuilder.new
     VIM.stub(:command)
     VIM.should_receive(:command).with('echon "OK"')
-    builder.build
+    subject.build
   end
 
 end
