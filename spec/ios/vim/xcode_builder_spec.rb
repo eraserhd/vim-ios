9dcdbb687f070d1f273bf62766892a90628f1582
Jason Felice <jason.m.felice@gmail.com>
2012-10-15 18:37:17 -0400

    Put stub of VIM.command in before(:each)

--- spec/ios/vim/xcode_builder_spec.rb
+++ spec/ios/vim/xcode_builder_spec.rb
@@ -3,16 +3,18 @@ require 'ios/vim'
 module VIM; end
 
 describe IOS::Vim::XcodeBuilder do
+  before(:each) do
+    VIM.stub(:command)
+  end
+
   it {should respond_to(:build)}
 
   it 'tells us it is building' do
-    VIM.stub(:command)
     VIM.should_receive(:command).with('echo "Building... "')
     subject.build
   end
 
   it 'tells us when it is done building' do
-    VIM.stub(:command)
     VIM.should_receive(:command).with('echon "OK"')
     subject.build
   end
